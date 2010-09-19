88d5e3096889b62bd62e8b44e15c22be8aece972
John Montgomery <john@littlespikeyland.com>
2010-09-19 18:25:34 +0100

    Now listens on serial port and sends growl notification
    Essentially the minimal amount needed to make this useful

--- doorbell.py
+++ doorbell.py
@@ -1,6 +1,8 @@
 from PicoRendezvous import PicoRendezvous
 from netgrowl import GrowlRegistrationPacket, GrowlNotificationPacket, GROWL_UDP_PORT
+import serial
 from socket import socket, AF_INET, SOCK_DGRAM
+import sys
 
 def send_grow_notification(password=None):
     reg = GrowlRegistrationPacket(password=password)
@@ -16,8 +18,23 @@ def send_grow_notification(password=None):
         s.sendto(reg.payload(), addr)
         s.sendto(notify.payload(), addr)
 
+def listen_on_serial_port(port):
+    ser = serial.Serial(port, 9600, timeout=1)
+    try:
+        while True:
+            line = ser.readline()
+            if line is not None:
+                line = line.strip()
+            if line == 'DING DONG':
+                send_grow_notification()
+    finally:
+        if ser:
+            ser.close()
+    
 
 if __name__ == '__main__':
-    # test we can send growl notifications
-    # to all net growl services on network
-    send_grow_notification()
+    if len(sys.argv) < 2:
+        print "Please specify a serial port"
+        sys.exit(-1)
+    port = sys.argv[1]
+    listen_on_serial_port(port)
