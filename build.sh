fe8698a9b0ecbc78251ecbb785a9359a45907092
John Montgomery <john@littlespikeyland.com>
2010-10-16 18:15:27 +0100

    Including serial in build.sh
    So we get a fulle ready-to-run app for the chumby

--- build.sh
+++ build.sh
@@ -2,8 +2,16 @@
 
 # copy files to dist directory
 # for installing on chumby
+# assumes dependencies are installed
+# into virtualenv within current directory.
+# including all dependencies to simply "installing"
+# on chumby
 if [ ! -d dist/gntp ]; then
     mkdir -p dist/gntp
 fi
+if [ ! -d dist/serial ]; then
+    mkdir -p dist/serial
+fi
 cp debugchumby *.py dist
 cp src/gntp/gntp/*.py dist/gntp
+cp lib/python2*/site-packages/serial/*.py dist/serial
